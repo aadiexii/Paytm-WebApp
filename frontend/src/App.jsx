4d3e69190864ebed494686998b692c62b762fa89
Shivam Sharma <shivam14052004@gmail.com>
2025-08-24 17:52:32 +0530

    Started with frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,23 @@
 import './App.css'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Signup from '../src/Routes/Signup.jsx'
+import Signin from '../src/Routes/Signin.jsx'
+import Dashboard from '../src/Routes/Dashboard.jsx'
+import SendMoney from '../src/Routes/SendMoney.jsx'
 
 function App() {
-  return <div>
-    Hello World
-  </div>
+  return (
+    <>
+      <BrowserRouter>
+         <Routes>
+            <Route path='/signup' element={<Signup/>}></Route>
+            <Route path='/signin' element={<Signin/>}></Route>
+            <Route path='/dashboard' element={<Dashboard/>}></Route>
+            <Route path='/send' element={<SendMoney/>}></Route>
+         </Routes>
+      </BrowserRouter>
+    </>
+  )
 }
 
 export default App
